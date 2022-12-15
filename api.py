9b32e696bbef7ea915e9fab105363eb69eacc822
aLL3H <82620750+aLL3H@users.noreply.github.com>
2022-12-15 15:36:46 -0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def retornarPong():
+    query = {'ping':'pong'}
+    return jsonify(query)
+
+
+
+app.run()
+
+
